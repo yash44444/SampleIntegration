31aa6793a477abf7ef393cb56c59bd84832b92ea
yash <yash01verma@gmail.com>
2023-03-09 18:30:09 +0000

    latest commit

---
+++ src/test/java/com/ibm/tests/TestCase.java
@@ -0,0 +1,15 @@
+package com.ibm.tests;
+
+import org.testng.annotations.Test;
+
+public class TestCase {
+
+    @Test(description="sample test case")
+    public void sampleTest(){
+
+        System.out.println("hello");
+
+    }
+
+
+}
---
+++ src/test/java/com/ibm/tests/TestCase2.java
@@ -0,0 +1,16 @@
+package com.ibm.tests;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+
+    @Test(description="sample test case")
+    public void sampleTest(){
+
+        System.out.println("hello again!");
+        System.out.println("hello again!");
+
+    }
+
+
+}
